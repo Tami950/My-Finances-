e883b0ba2db73fb4c12e82a1e63c015fb1ad2294
Tamara <tamara.dimaggio@gmail.com>
2025-10-11 21:57:28 +0200

    some adjustment

--- app/src/main/java/com/examplet/myfinances/ui/navigation/Route.kt
+++ app/src/main/java/com/examplet/myfinances/ui/navigation/Route.kt
@@ -2,6 +2,7 @@ package com.examplet.myfinances.ui.navigation
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ReceiptLong
 import androidx.compose.material.icons.outlined.AccountBalance
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Home
@@ -17,7 +18,7 @@ enum class Route(
     Dashboard("dashboard", R.string.tab_dashboard, Icons.Outlined.Home),
     Casa("casa", R.string.tab_home, Icons.Outlined.AccountBalance),
     Personale("personale", R.string.tab_mine, Icons.Outlined.AccountCircle),
-    Bollette("bollette", R.string.tab_bills, Icons.Outlined.ReceiptLong)
+    Bollette("bollette", R.string.tab_bills, Icons.AutoMirrored.Outlined.ReceiptLong)
 }
 
 val BottomBarItems = listOf(
