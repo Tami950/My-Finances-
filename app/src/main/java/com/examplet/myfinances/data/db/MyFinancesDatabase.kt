b941ac7dcc03c2e8891a02f2d24990c6930c6ab9
Tami950 <66872278+Tami950@users.noreply.github.com>
2026-08-30 15:40:01 +0200

    Register complete Casa Room schema

--- app/src/main/java/com/examplet/myfinances/data/db/MyFinancesDatabase.kt
+++ app/src/main/java/com/examplet/myfinances/data/db/MyFinancesDatabase.kt
@@ -2,14 +2,34 @@ package com.examplet.myfinances.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.examplet.myfinances.data.dao.HouseCategoryDao
+import com.examplet.myfinances.data.dao.HouseMonthAccountBalanceDao
+import com.examplet.myfinances.data.dao.HouseMonthDao
+import com.examplet.myfinances.data.dao.HouseMonthlyAllocationDao
+import com.examplet.myfinances.data.dao.MoneyAccountDao
 import com.examplet.myfinances.data.entity.HouseCategoryEntity
+import com.examplet.myfinances.data.entity.HouseMonthAccountBalanceEntity
+import com.examplet.myfinances.data.entity.HouseMonthEntity
+import com.examplet.myfinances.data.entity.HouseMonthlyAllocationEntity
+import com.examplet.myfinances.data.entity.MoneyAccountEntity
 
 @Database(
-    entities = [HouseCategoryEntity::class],
-    version = 1,
+    entities = [
+        MoneyAccountEntity::class,
+        HouseCategoryEntity::class,
+        HouseMonthEntity::class,
+        HouseMonthlyAllocationEntity::class,
+        HouseMonthAccountBalanceEntity::class
+    ],
+    version = 2,
     exportSchema = false
 )
+@TypeConverters(RoomConverters::class)
 abstract class MyFinancesDatabase : RoomDatabase() {
+    abstract fun moneyAccountDao(): MoneyAccountDao
     abstract fun houseCategoryDao(): HouseCategoryDao
+    abstract fun houseMonthDao(): HouseMonthDao
+    abstract fun houseMonthlyAllocationDao(): HouseMonthlyAllocationDao
+    abstract fun houseMonthAccountBalanceDao(): HouseMonthAccountBalanceDao
 }
