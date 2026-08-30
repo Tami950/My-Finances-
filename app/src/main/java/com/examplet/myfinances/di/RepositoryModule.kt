1b997f26ea45a8b1ee14a5afbf414c16c7ff836f
Tami950 <66872278+Tami950@users.noreply.github.com>
2026-08-30 14:20:28 +0200

    feat: bind house category repository

---
+++ app/src/main/java/com/examplet/myfinances/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.examplet.myfinances.di
+
+import com.examplet.myfinances.data.repository.HouseCategoryRepositoryImpl
+import com.examplet.myfinances.domain.repository.HouseCategoryRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindHouseCategoryRepository(
+        implementation: HouseCategoryRepositoryImpl
+    ): HouseCategoryRepository
+}
